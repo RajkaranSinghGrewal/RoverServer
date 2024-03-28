9ca9375f56d23d894d546ee4a61c885f2651d9c2
TomaicazyX PI 1 <rajkaran.singh.grewal5@gmail.com>
2024-03-28 21:22:41 +0530

    Removed the init code from manage.py

--- gas_detecting_rover_controller/manage.py
+++ gas_detecting_rover_controller/manage.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import netifaces
-import requests
-import time
 
 
-def get_ip_address(interface='wlan0'):
-    try:
-        addresses = netifaces.ifaddresses(interface).get(netifaces.AF_INET)
-        if addresses:
-            return addresses[0]['addr']
-        else:
-            return None
-    except ValueError as e:
-        print('Error:',e)
-        return None
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gas_detecting_rover_controller.settings")
     try:
@@ -33,22 +21,5 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    ip_address = None
-    while ip_address == None:
-        ip_address = get_ip_address()
-    data = {'ip_address':str(ip_address) + '.'}
-    print(data)
-    print("RESETTING")
-    try:
-        for i in range(0,5):
-            print(".")
-            time.sleep(1)
-        response = requests.get('http://10.0.0.25/ip_configure',data=data)
-        print("response:",response.status_code)
-        #while response.status_code != 200:
-        #    print("Error: ",response.status_code)
-        #    time.sleep(10)
-        #    response = requests.post('http://10.0.0.25/ip_configure',data=data)
-    except Exception as e:
-        print("Error:",e)
+
     execute_from_command_line(sys.argv)
